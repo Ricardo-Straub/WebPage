64cc2016b13101b854c33bd5ca15a77e9fd0811e
bratkartoffel49 <ricckk49@gmail.com>
2023-10-19 02:31:46 +0200

    change style

--- app/page.js
+++ app/page.js
@@ -3,28 +3,38 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-    <div className='w-fit bg-white grid grid-rows-{11} grid-cols-11'>
-      {
-        Array(121)
-          .fill(true)
-          .map(() => {
-            return <Coordinate/>
-          })
-      }
+    <div className='w-screen h-screen flex justify-center place-items-center'>
+      <Board/>
     </div>
+      
+      
     </>
+  )
+}
+
+function Board() {
+  return (
+    <div className='w-fit h-fit grid grid-rows-{10} grid-cols-12 border border-gray-300 border-opacity-25'>
+        {
+          Array(120)
+            .fill(true)
+            .map(() => {
+              return <Coordinate/>
+            })
+        }
+      </div>
   )
 }
 
 function Coordinate() {
   return (
     <>
-    <div className='bg-black w-8 h-8 relative'>
+    <div className=' w-8 h-8 relative opacity-25'>
       <div className='w-full h-full flex flex-col justify-center '>
-        <div className='bg-gray-300 w-8 h-px'></div>
+        <div className='bg-gray-300 w-8 h-0.5'></div>
       </div>
       <div className='w-full h-full flex flex-row justify-center absolute inset-0'>
-        <div className='bg-gray-300  w-px h-8'></div>
+        <div className='bg-gray-300  w-0.5 h-8 '></div>
       </div>
     </div>
     </>
