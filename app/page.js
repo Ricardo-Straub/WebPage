b93cb6d20507fd6f779ee7c37efc0e5ea2e975b2
bratkartoffel49 <ricckk49@gmail.com>
2023-10-19 14:46:12 +0200

    add circle component

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
+'use client';
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
   return (
@@ -14,9 +16,9 @@ export default function Home() {
 
 function Board() {
   return (
-    <div className='w-fit h-fit grid grid-rows-{10} grid-cols-12 border border-gray-300 border-opacity-25'>
+    <div className='w-fit h-fit grid grid-rows-{15} grid-cols-20 border border-gray-300 border-opacity-25'>
         {
-          Array(120)
+          Array(300)
             .fill(true)
             .map(() => {
               return <Coordinate/>
@@ -27,15 +29,18 @@ function Board() {
 }
 
 function Coordinate() {
+  const [isHovering, setIsHovering] = useState(false);
   return (
     <>
-    <div className=' w-8 h-8 relative opacity-25'>
+    <div onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)} 
+    className=' w-8 h-8 relative opacity-25'>
       <div className='w-full h-full flex flex-col justify-center '>
         <div className='bg-gray-300 w-8 h-0.5'></div>
       </div>
       <div className='w-full h-full flex flex-row justify-center absolute inset-0'>
         <div className='bg-gray-300  w-0.5 h-8 '></div>
       </div>
+      {isHovering && <div className='bg-gray-300 w-4 h-4 absolute inset-2 rounded-full'></div>}
     </div>
     </>
   )
