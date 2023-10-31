4a150e385151b7f326b7e111351e45303e7ae36c
bratkartoffel49 <ricckk49@gmail.com>
2023-10-31 23:50:44 +0100

    fix coordinate system

--- app/page.js
+++ app/page.js
@@ -16,30 +16,46 @@ export default function Home() {
 
 //use state in parent Board instead of Coordinate in order to calculate Convex hull
 function Board() {
-  const [isHoveringArr, setIsHoveringArr] = useState(Array(15).fill(Array(20).fill(false)));
-  const [isClickedArr, setIsClickedArr] = useState(Array(15).fill(Array(20).fill(false)));
-  const handleHover = (i) => {
-
+  const [isHoveringArr, setIsHoveringArr] = useState(Array(15).fill().map(() => Array(20).fill(false)));
+  const [isClickedArr, setIsClickedArr] = useState(Array(15).fill().map(() => Array(20).fill(false)));
+  const handleHover = (x, y) => {
+    const temp = isHoveringArr.slice();
+    temp[y][x] = !temp[y][x];
+    setIsHoveringArr(temp);
   };
+  const handleClick = (x, y) => {
+    const temp = isClickedArr.slice();
+    temp[y][x] = !temp[y][x];
+    setIsClickedArr(temp);
+  };
+  const arr = Array.from(Array(15), () => Array(20).fill(null));
+  
   return (
     <div className='w-fit h-fit grid grid-rows-{15} grid-cols-20 border border-gray-300 border-opacity-25'>
         {
-          Array(300)
-            .fill(true)
-            .map(() => {
-              return <Coordinate/>
-            })
-        }
+          arr.map((row, y) => (
+            row.map((col, x) => (
+              <Coordinate 
+                x={x}
+                y={y} 
+                isHovering={isHoveringArr[y][x]}  
+                isClicked={isClickedArr[y][x]}     
+                handleHover={() => handleHover(x, y)}
+                handleClick={() => handleClick(x, y)}
+              />
+            ))
+          ))
+            }
       </div>
   )
 }
 
-function Coordinate({isHovering, isClicked, handleHover, handleClick}) {
+function Coordinate({x, y, isHovering, isClicked, handleHover, handleClick}) {
   
   return (
     <>
-    <div onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)} 
-    onClick={() => setIsClicked(prev => !prev)} 
+    <div onMouseOver={handleHover} onMouseOut={handleHover} 
+    onClick={handleClick} 
     className=' w-8 h-8 relative opacity-25'>
       <div className='w-full h-full flex flex-col justify-center '>
         <div className='bg-gray-300 w-8 h-0.5'></div>
